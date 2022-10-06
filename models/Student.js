19d5645f11fa1763df4245a8354d7d347c5e5060
alexmayberry <alexclimbs@gmail.com>
2022-10-06 14:25:40 -0700

    added starter files. made userSchema

---
+++ models/Student.js
@@ -0,0 +1,47 @@
+const { Schema, model } = require('mongoose');
+const assignmentSchema = require('./Assignment');
+
+// Schema to create User model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      // TODO email validator
+    },
+    // unsure if this will give us an array of throught ids
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought',
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+  }
+);
+
+// Create a virtual property `commentCount` that gets the amount of comments per post
+userSchema.virtual('friendCount').get(function () {
+  return this.friends.length;
+});
+
+const User = model('user', userSchema);
+
+module.exports = User;
